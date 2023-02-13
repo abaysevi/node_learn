4ac890af21d2f2215bb551c881f78a757bf16bd5
Abay Sevi <abaysevi012@gmail.com>
2023-02-13 10:34:33 +0530

    new commit

--- learn_actual_node/app.js
+++ learn_actual_node/app.js
@@ -1,8 +1,26 @@
-const http =require("http");
 
-const server=http.createServer((req,res)=>{
-    console.log(req.headers);
-    // process.exit();
-});
+const express =require("express")
+const app=express();
+const bodyParser= require("body-parser")
+const path = require("path")
 
-server.listen(3000);
+const adminRoutes =require('./routes/admin')
+
+const shopRoutes = require("./routes/shop")
+
+app.use(express.static(path.join(__dirname,'./views')));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/admin",adminRoutes);
+
+app.use(shopRoutes);
+
+app.use((req,res,next)=>{
+    res.status(404)
+    res.sendFile(__dirname + '/views/error.html');
+})
+
+app.listen(3000,()=>{
+    console.log("port running on loaclhost 3000")
+})
---
+++ learn_actual_node/routes/admin.js
@@ -0,0 +1,18 @@
+const { application } = require("express");
+const express = require("express");
+const path = require("path");
+
+
+
+const router = express.Router()
+
+router.get('/add_product', (req, res, next) => {
+    res.sendFile(path.join(__dirname,"../","views","add_product.html"))
+})
+
+router.post('/product', (req, res, next) => {
+    console.log(req.body)
+    res.redirect('/');
+})
+
+module.exports = router;
